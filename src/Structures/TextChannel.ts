c1aa75ae4fbbf5c38ee226a864d9afd9f3d42c7b
Arnav Mishra <arnavdrsk7633@gmail.com>
2022-02-27 16:05:09 +0530

    Export TextChannel class and document it

--- src/Structures/TextChannel.ts
+++ src/Structures/TextChannel.ts
@@ -1,39 +1,58 @@
 //@ts-nocheck
-import { APIChannel } from 'discord-api-types'
-import { Channel } from '../../typings'
-import Client from './Client'
-import { fetch } from 'undici'
-import BaseChannel from './BaseChannel'
+import { APIChannel } from "discord-api-types";
+import { Channel } from "../../typings";
+import Client from "./Client";
+import { fetch } from "undici";
+import BaseChannel from "./BaseChannel";
+/**
+ * The TextChanell is the class which represents a guild text channel
+ * @extends BaseChannel
+ * @example
+ * ```
+ * Client.guilds.get("12345678901234567").channels.get("12345678901234567");
+ * ```
+ *
+ */
 class TextChannel extends BaseChannel implements Channel {
-	constructor(data: Channel) {
-		super()
-		const keys = Object.keys(data)
-		keys.forEach(key => {
-			this[key] = data[key]
-		})
-	}
-	async send(message: string) {
-		console.log(this.id)
-		const res = await fetch(
-			`https://discord.com/api/v10/channels/${this.id}/messages`,
-			{
-				body: JSON.stringify({
-					content: 'Hello, World!'
-				}),
-				headers: {
-					'User-Agent': 'undici/tej.js',
-					Authorization: `Bot ${this.client.token}`,
-					'Content-Type': 'application/json'
-				},
-				method: 'POST'
-			}
-		).catch(err => console.log(err))
-		const resp = await res.json()
-		if (resp.code) {
-			throw new Error(resp.message)
-		}
-		return Promise.resolve()
-	}
+  constructor(data: Channel) {
+    super();
+    const keys = Object.keys(data);
+    keys.forEach((key) => {
+      this[key] = data[key];
+    });
+  }
+  /**
+   *
+   * @param message string
+   * @example
+   * ```
+   * client.guilds.get("1234567890").channels.get("12345678901234567").send("Hello World!");
+   * ```
+   * @returns Promise<void>
+   *
+   */
+  async send(message: string) {
+    console.log(this.id);
+    const res = await fetch(
+      `https://discord.com/api/v10/channels/${this.id}/messages`,
+      {
+        body: JSON.stringify({
+          content: "Hello, World!",
+        }),
+        headers: {
+          "User-Agent": "undici/tej.js",
+          Authorization: `Bot ${this.client.token}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+    ).catch((err) => console.log(err));
+    const resp = await res.json();
+    if (resp.code) {
+      throw new Error(resp.message);
+    }
+    return Promise.resolve();
+  }
 }
 
-export default TextChannel
+export default TextChannel;
