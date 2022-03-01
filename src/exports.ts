c88fb34553431e8f115a7cd6968c86a824ca94e8
Arnav Mishra <arnavdrsk7633@gmail.com>
2022-03-01 20:47:14 +0530

    Export guild class

--- src/exports.ts
+++ src/exports.ts
@@ -2,4 +2,13 @@ import Client from "./Structures/Client";
 import { IClient, IClientOptions, IGuild } from "../typings";
 import BaseChannel from "./Structures/BaseChannel";
 import TextChannel from "./Structures/TextChannel";
-export { Client, IClient, IClientOptions, IGuild, BaseChannel, TextChannel };
+import Guild from "./Structures/Guild";
+export {
+  Client,
+  IClient,
+  IClientOptions,
+  IGuild,
+  BaseChannel,
+  TextChannel,
+  Guild,
+};
